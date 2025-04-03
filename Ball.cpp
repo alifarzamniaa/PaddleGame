7923926b605c48c36b3d5b1fa002f081a11ae0a4
Ali Farzamnia <alifarzamniadev@gmail.com>
2025-04-03 17:59:25 +0330

    Changed the bouncing system
    making it be based on where the ball hit the surface rather than using dot product (its because we have flat surfaces)

--- Ball.cpp
+++ Ball.cpp
@@ -28,20 +28,15 @@ bool Ball::GetFloorHit()
 }
 void Ball::Bouncing(float delta, const PlayGround& Area)
 {
-	if (ball.getPosition().x < Area.GetPosition().x - (Area.GetWidth() / 2)) // origin is on center so we need the half of the width to get left side
+	if (ball.getPosition().x < Area.GetPosition().x - (Area.GetWidth() / 2) ||  // origin is on center so we need the half of the width to get left side
+		ball.getPosition().x + 2 * radius > Area.GetWidth() + (Area.GetOffset().x / 2)
+		) 
 	{
-		float DotProduct = (BallVel.x * 0) + (BallVel.y * 1);
-		BallVel = (2.f * (DotProduct * sf::Vector2f(0, 1))) - BallVel;
-	}
-	if (ball.getPosition().x + 2 * radius > Area.GetWidth() + (Area.GetOffset().x / 2))
-	{
-		float DotProduct = (BallVel.x * 0) + (BallVel.y * -1);
-		BallVel =(2.f * (DotProduct * sf::Vector2f(0, -1))) - BallVel;
+		BallVel.x *= -1;
 	}
 	if (ball.getPosition().y < Area.GetPosition().y - (Area.GetHeight() / 2))
 	{
-		float DotProduct = (BallVel.x * 1) + (BallVel.y * 0);
-		BallVel = (2.f * (DotProduct * sf::Vector2f(1, 0))) - BallVel;
+		BallVel.y *= -1;
 	}
 	if (ball.getPosition().y + 2 * radius > Area.GetHeight() + (Area.GetOffset().y / 2 + Area.GetTopOffset()))
 	{
@@ -58,9 +53,27 @@ void Ball::CheckPaddleCollision(const Paddle& paddle)
 	   BallVel.y > 0.f
 		)
 		{
-			sf::Vector2f paddleVec =  sf::Vector2f(paddle.GetWidth(),0).normalized();
-			float DotProduct = BallVel.x * paddleVec.x + BallVel.y * paddleVec.y;
-			BallVel =  (2.f * (DotProduct * paddleVec)) - BallVel;
+			float PaddleCenter = (paddle.GetPos().x + paddle.GetWidth() / 2.f);
+			float HitPos = (ball.getPosition().x - PaddleCenter) / (paddle.GetWidth() /2);
+			HitPos = std::clamp(HitPos,-1.f,1.f);
+			
+			if (HitPos < -0.4f) //if it hits the left side
+			{
+				if(BallVel.x > 0)
+					BallVel.x *= -1;
+				if(BallVel.x < 0)
+					BallVel.x *= 1;
+			}
+				
+			else if(HitPos > 0.4f)
+			{
+				if (BallVel.x > 0)
+					BallVel.x *= 1;
+				if (BallVel.x < 0)
+					BallVel.x *= -1;
+			}
+			
+			BallVel.y *= -1;
 			
 		}
 }
@@ -74,9 +87,24 @@ void Ball::CheckBoxCollision(std::vector<Box>& boxes)
 			ball.getPosition().y + 2 * radius > boxes[i].GetPos().y
 			)
 		{
-			sf::Vector2f paddleVec = sf::Vector2f(boxes[i].GetSize().x, 0).normalized();
-			float DotProduct = BallVel.x * paddleVec.x + BallVel.y * paddleVec.y;
-			BallVel = (2.f * (DotProduct * paddleVec)) - BallVel;
+			float PaddleCenter = (boxes[i].GetPos().x + boxes[i].GetSize().x / 2.f);
+			float HitPos = (ball.getPosition().x - PaddleCenter) / (boxes[i].GetSize().x / 2);
+			if (HitPos < -0.4f) //if it hits the left side
+			{
+				if (BallVel.x > 0)
+					BallVel.x *= -1;
+				if (BallVel.x < 0)
+					BallVel.x *= 1;
+			}
+
+			else if (HitPos > 0.4f)
+			{
+				if (BallVel.x > 0)
+					BallVel.x *= 1;
+				if (BallVel.x < 0)
+					BallVel.x *= -1;
+			}
+			BallVel.y *= -1;
 			boxes.erase(boxes.begin() + i);
 		}
 	}
